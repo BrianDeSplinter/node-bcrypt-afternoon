153168bc6ed5b18e8cfe32d32c2c88b3b8317676
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-03 09:07:31 -0600

    setup

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require ('express')
+const session = require ('express-session')
+const massive = require ('massive')
+const SERVER_PORT = 4000
+
+const app = express()
+
+app.listen(SERVER_PORT, )
